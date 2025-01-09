70e8cf6e3d865e5aa031e5ff2055c9ccbb4b4997
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-10 01:53:59 +0530

    mid commit

--- Blog/startBlog/urls.py
+++ Blog/startBlog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from . import views #importing views.py file from thecurrent dir
+# from . import views #importing views.py file from thecurrent dir
+from .views import HomeView
 urlpatterns = [
-    path('', views.home, name="home"), #'' is blank so that the url doesnt have /home or anything like that, and views.home is calling for the home function from the view.py file
+    # path('', views.home, name="home"), #'' is blank so that the url doesnt have /home or anything like that, and views.home is calling for the home function from the view.py file
+    path('', HomeView.as_view(), name="home"),
 ]
--- Blog/startBlog/views.py
+++ Blog/startBlog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {}) #making a new view called home that calles for home.html file and {} is to give any parameters
+# def home(request):
+#     return render(request, 'home.html', {}) #making a new view called home that calles for home.html file and {} is to give any parameters
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
