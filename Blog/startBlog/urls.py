cae466743d7dc872f99d4bdc3f32ef78c13a5e9a
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-10 16:06:51 +0530

    Completed Video 2

--- Blog/startBlog/urls.py
+++ Blog/startBlog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # from . import views #importing views.py file from thecurrent dir
-from .views import HomeView
+from .views import HomeView, PostView
 urlpatterns = [
     # path('', views.home, name="home"), #'' is blank so that the url doesnt have /home or anything like that, and views.home is calling for the home function from the view.py file
-    path('', HomeView.as_view(), name="home"),
+    path('', HomeView.as_view(), name="home"), #using the class views
+    path('post/<int:pk>', PostView.as_view(), name="post-detail"),
 ]
--- Blog/startBlog/views.py
+++ Blog/startBlog/views.py
@@ -5,6 +5,13 @@ from .models import Post
 # def home(request):
 #     return render(request, 'home.html', {}) #making a new view called home that calles for home.html file and {} is to give any parameters
 
+#ListView returns all the posts
+#DetailView return the whole detail of one single post
+
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class PostView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
