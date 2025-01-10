0cf51ca8f98e3e3a3e29696706abc2f368f2f795
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-10 20:04:20 +0530

    mid commit

--- Blog/startBlog/urls.py
+++ Blog/startBlog/urls.py
@@ -4,5 +4,5 @@ from .views import HomeView, PostView
 urlpatterns = [
     # path('', views.home, name="home"), #'' is blank so that the url doesnt have /home or anything like that, and views.home is calling for the home function from the view.py file
     path('', HomeView.as_view(), name="home"), #using the class views
-    path('post/<int:pk>', PostView.as_view(), name="post-detail"),
+    path('post/<int:pk>', PostView.as_view(), name="post-detail"), #the <int:pk> is passing the primary key as a parameter
 ]
