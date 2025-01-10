6abea60566cb14027087f3b9ab3fb381b38ea203
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-10 21:03:15 +0530

    Completed Video 3

--- Blog/startBlog/models.py
+++ Blog/startBlog/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import User #importing users
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tags = models.CharField(max_length=255, default="Posts.") #as this field is added later to make sure that the old posts dont have any null values we pass some default value that can be stored
     author = models.ForeignKey(User, on_delete=models.CASCADE) #If user is deleted then this removes all the posts from the user
     body = models.TextField()
+    
 
     def __str__(self):
         return self.title + " | " + str(self.author)
