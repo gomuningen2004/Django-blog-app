78ca95930ead9a87dafac626f77e24aa09f70dee
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-10 23:20:20 +0530

    Completed Video 6

--- Blog/startBlog/urls.py
+++ Blog/startBlog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from . import views #importing views.py file from thecurrent dir
-from .views import HomeView, PostView, AddPostView
+from .views import HomeView, PostView, AddPostView, UpdatePostView
 urlpatterns = [
     # path('', views.home, name="home"), #'' is blank so that the url doesnt have /home or anything like that, and views.home is calling for the home function from the view.py file
     path('', HomeView.as_view(), name="home"), #using the class views
     path('post/<int:pk>', PostView.as_view(), name="post-detail"), #the <int:pk> is passing the primary key as a parameter
     path('add_post/', AddPostView.as_view(), name="add-post"),
+    path('edit_post/<int:pk>', UpdatePostView.as_view(), name="edit-post"),
 ]
--- Blog/startBlog/views.py
+++ Blog/startBlog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, EditForm
 
 # def home(request):
 #     return render(request, 'home.html', {}) #making a new view called home that calles for home.html file and {} is to give any parameters
@@ -22,3 +22,9 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__' #to get all fields in the Post method if needed separately then we can just use (field names separated by ,)
+
+class UpdatePostView(UpdateView):
+    model = Post
+    form_class = EditForm
+    template_name = 'update_post.html'
+    # fields = ['title', 'body'] #when having form_class we cant have fields
