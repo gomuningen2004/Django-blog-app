a0bd21fd3753fad67ea2f49fef1b2de42a244f67
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-18 14:18:28 +0530

    Completed Video 25

--- Blog/members/forms.py
+++ Blog/members/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -27,3 +27,12 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+class PasswordChangingForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control', 'type' : 'password'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control', 'type' : 'password'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'form-control', 'type' : 'password'}))
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'new_password1', 'new_password2')
