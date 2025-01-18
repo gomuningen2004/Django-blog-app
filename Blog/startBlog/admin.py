04599b54c050fffbd15c9650949def316cdfd711
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-18 15:04:35 +0530

    Completed Video 27

--- Blog/startBlog/admin.py
+++ Blog/startBlog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, Profile
 
 admin.site.register(Post) #access posts from the admin area
 admin.site.register(Category)
+admin.site.register(Profile)
