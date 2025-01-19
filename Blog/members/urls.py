d4744b832b53fcdaa97772b26efde7c793039147
gomuningen2004 <govardhan.personal.new@gmail.com>
2025-01-19 22:39:48 +0530

    Completed Video 30 and 31

--- Blog/members/urls.py
+++ Blog/members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordsChangeView
+from .views import UserRegisterView, UserEditView, PasswordsChangeView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name="edit-profile"),
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
     path('password/', PasswordsChangeView.as_view(template_name='registration/change-password.html')),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name="user-profile"),
 ]
--- Blog/members/views.py
+++ Blog/members/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm, PasswordChangingForm
 from django.contrib.auth.views import PasswordChangeView
+from django.views.generic import DetailView
+from startBlog.models import Profile
+
 
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
@@ -21,3 +24,16 @@ class UserEditView(generic.UpdateView):
 class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangingForm
     success_url = reverse_lazy('home')
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
